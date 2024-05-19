c7a934ab4ef703cc7e724e428358c5a876c64152
Onisim Albarov <Onisim93@gmail.com>
2024-05-19 19:58:42 +0400

    add ArrayTasks

---
+++ src/main/java/org/example/practice/tasks/ArrayTasks.java
@@ -0,0 +1,43 @@
+package org.example.practice.tasks;
+
+public class ArrayTasks {
+
+
+    /*
+      A = [a, a, t, e, f, i, j]
+      B = [t, g, g, i, k, f]
+      difference = [a, e, g, j, k]
+    */
+    public String[] differenceBetweenTwoArrays(String[] firstArray, String[] secondArray) {
+
+        return new String[0];
+    }
+
+    /*
+        123 => [1,2,3]
+        1 => [1]
+        8675309 => [8,6,7,5,3,0,9]
+    */
+    public int[] numberToArray(int number) {
+
+        return new int[0];
+    }
+
+
+    /*
+        n = 0  ==> [1]        # [2^0]
+        n = 1  ==> [1, 2]     # [2^0, 2^1]
+        n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+    */
+    public long[] powersOf2(int n) {
+        return  new long[0];
+    }
+
+    /*
+        1.A=[1,2,3,4]⇒D=[1,1,1]
+        2.A=[1,3,4,10]⇒D=[2,1,6]
+    */
+    public int[] maximumDifferentDifferences(int[] source) {
+        return new int[0];
+    }
+}
